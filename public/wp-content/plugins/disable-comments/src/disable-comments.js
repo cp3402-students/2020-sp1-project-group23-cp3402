0bba4337c917c91556a31b3e8978af2447163c66
Tim <timenglart@gmail.com>
2020-04-28 18:55:16 +1000

    remove public folder for now

--- public/wp-content/plugins/disable-comments/src/disable-comments.js
+++
@@ -1,9 +0,0 @@
-wp.domReady(() => {
-	if(wp.blocks) {
-		wp.blocks.getBlockTypes().forEach((block) => {
-			if (disable_comments.disabled_blocks.includes(block.name)) {
-				wp.blocks.unregisterBlockType(block.name);
-			}
-		});
-	}
-});
