2cc8a54e6f79387e985d4e8b1ff173cbd60a1b79
jordanMellish <jordan.mellish@my.jcu.edu.au>
2020-05-28 17:14:44 +1000

    change colour of heading

--- footer.php
+++ footer.php
@@ -15,7 +15,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 <?php get_template_part( 'sidebar-templates/sidebar', 'footerfull' ); ?>
 
-<div class="wrapper" id="wrapper-footer">
+<div class="wrapper bg-primary" id="wrapper-footer">
 
 	<div class="<?php echo esc_attr( $container ); ?>">
 
